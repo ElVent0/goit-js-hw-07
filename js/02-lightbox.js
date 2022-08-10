7c5836ee145496a4fecc1263031c758adffb5407
Vasyl Tarasyuk <vasfreelan@gmail.com>
2022-08-10 12:45:22 +0300

    2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,37 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+// Change code below this line 
 
-console.log(galleryItems);
+const galleryList = document.querySelector(".gallery");
+const galleryMarkup = createGalleryItems(galleryItems);
+
+galleryList.insertAdjacentHTML("beforeend", galleryMarkup);
+
+function createGalleryItems(images) {
+    return images.map(item => {
+    return `
+  <a class="gallery__item" href="${item.original}">
+    <img 
+        class="gallery__image" 
+        src="${item.preview}" 
+        alt="${item.description}" 
+        />
+  </a>
+`;
+    }).join("");
+};
+
+let lightbox;
+
+galleryList.addEventListener("click", (event) => {
+    if(!event.target.classList.contains('gallery__image')) {
+        return;
+    };
+
+    event.preventDefault();
+
+    lightbox = new SimpleLightbox('.gallery a', {
+        captionDelay: 250,
+        captionsData: 'alt',
+    });
+});
+
